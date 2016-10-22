51cc6f88016a461543cb4afce386de15ce32e455
Ken Li <iliawnek@gmail.com>
2016-10-22 14:26:00 +0100

    Initial commit

---
+++ wordladder/Main.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ program to find word ladder with shortest path (i.e. minimum number edges)
+ */
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		long start = System.currentTimeMillis();
+
+		String inputFileName = args[0]; // dictionary
+		String word1 = args[1]; // first word
+		String word2 = args[2]; // second word
+  
+		FileReader reader = new FileReader(inputFileName);
+		Scanner in = new Scanner(reader);
+		
+		// read in the data here
+
+        // create graph here
+
+        reader.close();
+
+        
+		// do the work here
+		
+		// end timer and print total time
+		long end = System.currentTimeMillis();
+		System.out.println("\nElapsed time: " + (end - start) + " milliseconds");
+	}
+
+}
